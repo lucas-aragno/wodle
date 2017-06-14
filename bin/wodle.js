169b7de82d029027fe0094c9db561d7feb5168cd
lucas <lucas.aragno157@gmail.com>
2017-06-14 12:44:38 -0300

    :art: wodle.js

--- bin/wodle.js
+++ bin/wodle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 const program = require('commander')
-const thoth = require('../src/index.js')
+const wodle = require('../src/index.js')
 
 program
   .version('1.0.0')
   .arguments('<site_name>')
   .action((site_name) => {
-    thoth.create(site_name)
+    wodle.create(site_name)
   })
   .parse(process.argv)
